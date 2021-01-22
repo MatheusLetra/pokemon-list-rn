4bbf55a142730318a9b3d4b02b5ebd72b21ef62e
MatheusLetra <jrtyuw@gmail.com>
2021-01-22 08:36:35 -0300

    Commit 22-01-2021

--- src/components/pokemon.js
+++ src/components/pokemon.js
@@ -1,5 +1,5 @@
 import React, {useEffect,useState} from  'react'
-import {Modal,View,Image,Text, TouchableOpacity} from 'react-native'
+import {Modal,View,Image,Text, TouchableOpacity, StyleSheet} from 'react-native'
 
 export default function Pokemon (props) {
 
@@ -9,7 +9,7 @@ export default function Pokemon (props) {
     const {name, url} = props.data.item
     const pokemonNumber = url.replace('https://pokeapi.co/api/v2/pokemon/','').replace('/','')
     const imageURL = 'https://pokeres.bastionbot.org/images/pokemon/'+ pokemonNumber + '.png'
-
+    
     useEffect(() => {
         fetch('https://pokeapi.co/api/v2/pokemon/' + String(name),{
             method: 'GET',
@@ -23,6 +23,9 @@ export default function Pokemon (props) {
             })
     },[])
 
+    const getAbilities = () => {
+        return 'Overgrow, Speed'
+    }
     const showPokemonDetails = () => {
         setModalVisible(!modalVisible)
     }
@@ -35,36 +38,72 @@ export default function Pokemon (props) {
                 animationType="slide"
                 transparent={true}
                 visible={modalVisible}>
-                <TouchableOpacity style={{flex: 1, backgroundColor: 'rgba(0, 0, 0, 0.7)'}}
+                <TouchableOpacity style={styles.outDetails}
                     onPress={showPokemonDetails}>
                     <View>
                     </View>
                 </TouchableOpacity>
-                <View style={{flex: 3, backgroundColor: '#f0f0ff', alignItems: 'center', justifyContent:'center'}}>
-                    <Image style={{width: 150, height: 150}}
+                <View style={styles.containerDetailsModal}>
+                    <Image style={styles.imageModal}
                         source={{uri: imageURL}}/>
-                    <Text style={{marginTop: 40, fontWeight: 'bold', fontSize: 30}}>
-                        {'#' + pokemonNumber + ' - ' + name.toUpperCase()}</Text>  
-                    {pokemonAbilities.map((abilitie) => {
-                            return(
-                                <Text>{abilitie.name}</Text>
-                            )
-                    })}
+                    <Text style={styles.textNameModal}>
+                        {'#' + pokemonNumber + ' - ' + name.toUpperCase()}</Text> 
+                    <Text style={styles.modalAbilities}>Habilities: {getAbilities()}</Text> 
                 </View>
-                <TouchableOpacity style={{flex: 1, backgroundColor: 'rgba(0, 0, 0, 0.7)'}}
+                <TouchableOpacity style={styles.outDetails}
                     onPress={showPokemonDetails}>
                     <View>
                     </View>
                 </TouchableOpacity>
             </Modal>
             <TouchableOpacity onPress={showPokemonDetails}>
-                <View style={{alignItems:'center', marginVertical: 20, marginHorizontal: 20, backgroundColor: '#EE82EE'}}>
-                    <Image style={{ width:75, height: 75}}
+                <View style={styles.mainContainer}>
+                    <Image style={styles.mainImage}
                         source={{uri: imageURL}}/>
-                    <Text style={{marginTop: 15}}>{'#' + pokemonNumber }</Text>
+                    <Text style={styles.mainText}>{'#' + pokemonNumber }</Text>
                 </View>
             </TouchableOpacity>
         </View>
     )
 
 }
+
+const styles = StyleSheet.create({
+    outDetails : {
+        flex: 1, 
+        backgroundColor: 'rgba(0, 0, 0, 0.7)'
+    },
+    containerDetailsModal:{
+        flex: 3, 
+        backgroundColor: '#f0f0ff', 
+        alignItems: 'center', 
+        justifyContent:'center'
+
+    },
+    imageModal: {
+        width: 150, 
+        height: 150
+    },
+    textNameModal:{
+        marginTop: 40, 
+        fontWeight: 'bold', 
+        fontSize: 30
+    },
+    mainContainer:{
+        alignItems:'center', 
+        marginVertical: 20, 
+        marginHorizontal: 20, 
+        backgroundColor: '#EE82EE'
+    },
+    mainImage:{
+        width:75, 
+        height: 75
+    },
+    mainText:{
+        marginTop: 15
+    },
+    modalAbilities:{
+        color: 'red',
+        fontSize: 20
+    }
+})
